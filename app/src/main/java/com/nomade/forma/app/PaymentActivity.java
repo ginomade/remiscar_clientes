2be57ec7741e2c7f866546a6fa78d2b259244afc
Gino Ghiotto <gino.ghiotto@globallogic.com>
2019-03-13 15:45:46 -0300

    prueba exitosa

--- app/src/main/java/com/nomade/forma/app/PaymentActivity.java
+++ app/src/main/java/com/nomade/forma/app/PaymentActivity.java
@@ -3,32 +3,19 @@ package com.nomade.forma.app;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.mercadopago.android.px.configuration.PaymentConfiguration;
+import com.mercadopago.android.px.configuration.AdvancedConfiguration;
 import com.mercadopago.android.px.core.MercadoPagoCheckout;
-import com.mercadopago.android.px.core.PaymentProcessor;
-import com.mercadopago.android.px.internal.datasource.MercadoPagoPaymentProcessor;
-import com.mercadopago.android.px.internal.util.JsonUtil;
-import com.mercadopago.android.px.model.Item;
 import com.mercadopago.android.px.model.Payment;
-import com.mercadopago.android.px.model.Site;
-import com.mercadopago.android.px.model.Sites;
 import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
-import com.mercadopago.android.px.preferences.CheckoutPreference;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PaymentActivity extends AppCompatActivity {
 
+    private static final String PREFERENCE_ID = "99103401-f93caed8-88f0-4e98-a9f7-6c01edc72e10";
+    private static final String PUBLIC_KEY = "TEST-ad365c37-8012-4014-84f5-6c895b3f8e0a";
     private static int REQUEST_CODE = 11;
     Context context;
 
@@ -43,23 +30,44 @@ public class PaymentActivity extends AppCompatActivity {
     // Método ejecutado al hacer clic en el botón
     public void submit(View view) {
 
-/*.setPublicKey("TEST-ad365c37-8012-4014-84f5-6c895b3f8e0a")
-                .setCheckoutPreferenceId("150216849-ceed1ee4-8ab9-4449-869f-f4a8565d386f")*/
-        Site site = Sites.ARGENTINA;
-        List<Item> items = new ArrayList<>();
-        BigDecimal precio = new BigDecimal(150);
-        Item unItem = new Item.Builder("viaje", 1, precio).build();
-        items.add(unItem);
-        CheckoutPreference preference = new CheckoutPreference.Builder(site, "pepe@gmail.com", items).build();
-        PaymentProcessor paymentProcessor = new MercadoPagoPaymentProcessor();
-        PaymentConfiguration paymentConf = new PaymentConfiguration.Builder(paymentProcessor).build();
-        startMercadoPagoCheckout(preference, paymentConf);
-
+        String checkoutPreferenceId = PREFERENCE_ID;
+        startMercadoPagoCheckout(checkoutPreferenceId);
 
     }
 
-    private void startMercadoPagoCheckout(CheckoutPreference checkoutPreference, PaymentConfiguration paymentConf) {
-        new MercadoPagoCheckout.Builder("TEST-ad365c37-8012-4014-84f5-6c895b3f8e0a", checkoutPreference, paymentConf).build()
+    /*
+        curl -X POST \
+                'https://api.mercadopago.com/checkout/preferences?access_token=TEST-8003123851032516-101716-bc39e29b28b102576ae57cde2a51a815-99103401' \
+                -H 'Content-Type: application/json' \
+                -d '{
+                "items": [
+        {
+            "title": "remiscar",
+                "description": "1 viaje",
+                "quantity": 1,
+                "currency_id": "ARS",
+                "unit_price": 120.0
+        }
+               ],
+                       "payer": {
+            "email": "payer@email.com"
+        },
+                "payment_methods": {
+            "excluded_payment_types":[
+            {"id":"atm"}
+                ],
+            "installments": 1
+        }
+    }'
+    */
+
+    private void startMercadoPagoCheckout(String checkoutPreferenceId) {
+        final AdvancedConfiguration advancedConfiguration =
+                new AdvancedConfiguration.Builder()
+                        .setBankDealsEnabled(false)
+                        .build();
+        new MercadoPagoCheckout.Builder(PUBLIC_KEY, checkoutPreferenceId)
+                .setAdvancedConfiguration(advancedConfiguration).build()
                 .startPayment(PaymentActivity.this, REQUEST_CODE);
         ((TextView) findViewById(R.id.mp_results)).setText("Pago iniciado");
     }
@@ -77,7 +85,7 @@ public class PaymentActivity extends AppCompatActivity {
                         && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
                     final MercadoPagoError mercadoPagoError =
                             (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
-                    ((TextView) findViewById(R.id.mp_results)).setText("Error: " +  mercadoPagoError.getMessage());
+                    ((TextView) findViewById(R.id.mp_results)).setText("Error: " + mercadoPagoError.getMessage());
                     //Resolve error in checkout
                 } else {
                     //Resolve canceled checkout
