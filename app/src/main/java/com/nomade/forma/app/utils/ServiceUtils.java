0504a7d200cc709c11707848b40fee8f34be7869
Gino Ghiotto <gino.ghiotto@globallogic.com>
2019-11-08 17:26:28 -0300

    fix envio de version a server

--- app/src/main/java/com/nomade/forma/app/utils/ServiceUtils.java
+++ app/src/main/java/com/nomade/forma/app/utils/ServiceUtils.java
@@ -1,6 +1,8 @@
 package com.nomade.forma.app.utils;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.Log;
 
 import com.google.gson.JsonObject;
@@ -57,10 +59,25 @@ public class ServiceUtils {
                 });
     }
 
+    public static String getVersionName(Context context){
+        String version = "";
+        try {
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            version = pInfo.versionName;
+            int versionCode = pInfo.versionCode;
+            Log.d("Remiscar", "Version Name : "+version + "\n Version Code : "+versionCode);
+        } catch(PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Log.d("Remiscar", "PackageManager Catch : "+e.toString());
+        }
+        return version;
+    }
+
     public static void validarImei(Context context) {
         Ion.with(context)
                 .load(url_celu_bloqueado)
                 .setBodyParameter("IMEI", SharedPrefsUtil.getInstance(context).getString("imei", ""))
+                .setBodyParameter("version", getVersionName(context))
                 .asJsonObject()
                 .setCallback(new FutureCallback<JsonObject>() {
                     @Override
